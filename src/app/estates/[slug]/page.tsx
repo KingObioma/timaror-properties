a87b11aaa333f5064cf75a6a1fa7c5ec06f282a8
king <kelvinozak0@gmail.com>
2026-03-22 21:03:24 +0100

    first commit

--- src/app/estates/[slug]/page.tsx
+++ src/app/estates/[slug]/page.tsx
@@ -89,7 +89,7 @@ export default async function EstatePage({ params }: Props) {
                 d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
               />
             </svg>
-            {estate.site}
+            {estate.sir}
           </p>
         </div>
       </section>
