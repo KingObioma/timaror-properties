18f2a271b86b54594a2acbde374980bb43880e5d
king <kelvinozak0@gmail.com>
2026-03-22 21:10:41 +0100

    first commit

--- src/app/estates/[slug]/page.tsx
+++ src/app/estates/[slug]/page.tsx
@@ -108,7 +108,7 @@ export default async function EstatePage({ params }: Props) {
               { label: "Land Title", value: estate.title, sub: null },
               {
                 label: "Payment Plan",
-                value: estate.paymentPlan.duration,
+                value: estate.paymentPlans.duration,
                 sub: "installment plan",
               },
               {
